7d0523e12e309846fb850611257490577e6848d9
Larry Augustin <Larry Augustin>
2016-12-09 11:07:34 -0800

    Initial dictionary class

---
+++ dictionary.cpp
@@ -0,0 +1,5 @@
+#include "dictionary.h"
+
+// Read the words in and create the dictionary
+Dictionary::Dictionary(istream *input) {
+}
---
+++ dictionary.h
@@ -0,0 +1,13 @@
+#ifndef DICTIONARY_H
+#define DICTIONARY_H
+
+#include "dictionary.h"
+#include <istream.h>
+
+// Dictionary stores all the legal scrabble words
+class Dictionary {
+ public:
+  Dictionary(istream *input);
+};
+
+#endif
